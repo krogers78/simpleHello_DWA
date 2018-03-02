558e428963d79239503f17b97db752928db1bcfc
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-02 13:29:18 -0500

    Changed JSON object

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
+  res.status(200).json({
+    msg: 'pls work',
+    author: 'kloe'
+  });
 });
 
 app.listen(port, () => console.log(`Server active on port: ${port}`));
