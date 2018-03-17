4173a1b55056a4714c21e7a48364791ee59f75ee
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-16 22:35:47 -0400

    Moved back to req.body

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res, next) => {
 
 app.post('/webhook', (req, res, next) => {
   const { author } = req.body;
-  console.log("test", req.body.author);
+  console.log("test", req.body);
   res.send('Page loaded successfully');
 });
 
